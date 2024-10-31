6f1f59da09f8300e81241037d52fd5069f55cfb8
ItLena <ebordinghaus@gmail.com>
2024-10-31 14:38:55 +0100

    dark theme installed

--- hogwart/src/App.jsx
+++ hogwart/src/App.jsx
@@ -1,17 +1,35 @@
 // import { useState } from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { useRoutes } from 'react-router-dom'
 import NavBar from '../src/components/Navbar'
+import Home from '../src/pages/Home'
 
 import './App.css'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function App() {
   // const [count, setCount] = useState(0)
+  let pages = useRoutes([
+    { path: "/", element: <Home /> },
+    // { path: "/cart", element: <Cart cartItems={cartItems} /> }
+
+  ])
 
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+  
     <NavBar/>
-     <h1>Hello Hogwart</h1>
-        
-    </>
+     
+        {pages}
+     
+    </ThemeProvider>
   )
 }
 
---
+++ hogwart/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () =>{
+    return(
+        <>
+        <h1>Hello Hogwart</h1>
+        </>
+    )
+}
+export default Home
